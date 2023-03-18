fcffa0ac2c9af896852c9215d753ed470afdf950
Lenine Junior <lenine_negocios@hotmail.com>
2023-03-18 12:26:40 -0300

    02

---
+++ 02/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- 
+        Você foi contratado para desenvolver um conversor de unidades, onde o usuario poderá inserir valores em milimetros, centimetros, metros ou kilometros e a aplicação deverá trazer a informação do valor correspondente nas demais unidades.
+o programa deve seguir as restrições abaixo:
+- O programa deve solicitar ao usuario que informe qual a unidade base do valor a ser inserido;
+- O programa não deve permitir o avanço das etapas caso o usuario digite uma unidade inválida;
+- O programa deve solicitar ao usuario o valor da unidade base;
+- O programa não deve permitir o avanço das etapas caso o usuario digite um valor não numerico;
+- O programa deverá imprimir os valores correspondendtes em todas as unidades disponiveis no sistema, exceto aquela que o usuario inseriu 
+-->
+<?php 
+    //unidades válidas para o sistema
+    $unidades = [
+        'mm' => 1,
+        'cm' => 10,
+        'm' => 1000,
+        'km'=> 1000000,
+    ];
+    //texto unidade validas
+    $unidadesValidas = implode(', ',array_keys($unidades));
+
+    //solicta a unidade base para o usuario
+    $unidadeBase = readline('Digite a unidade base('.$unidadesValidas.'):');
+?>
+</body>
+</html> 
